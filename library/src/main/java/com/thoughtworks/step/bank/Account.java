a4c5ddd1cf6d8289adaf92582fc85b65655d03f1
yogiraj_tambake <ytambake@gmail.com>
2018-03-20 15:28:02 +0530

    Added functionality to credit balance in account.

--- library/src/main/java/com/thoughtworks/step/bank/Account.java
+++ library/src/main/java/com/thoughtworks/step/bank/Account.java
@@ -9,11 +9,22 @@ public class Account {
     this.balance = balance;
   }
 
-  public int getBalance() {
+  public double getBalance() {
     return balance;
   }
 
   public String getAccountNumber() {
     return accountNumber;
+  }
+
+  public boolean doesHaveMinimumBalance() {
+    if (balance>=1000){
+      return true;
+    }
+    return false;
+  }
+
+  public double credit(int creditAmount) {
+    return balance+=creditAmount;
   }
 }
--- library/src/test/java/AccountTest.java
+++ library/src/test/java/AccountTest.java
@@ -9,11 +9,24 @@ public class AccountTest{
   @Test
   public void checkBalance(){
     Account acc = new Account("12345",10000);
-    assertThat(acc.getBalance(),is(10000));
+    assertThat(acc.getBalance(),is(10000.0));
   }
   @Test
   public void checkAccountNumber(){
     Account acc = new Account("12345",10000);
     assertThat(acc.getAccountNumber(),is("12345"));
+  }
+  @Test
+  public void checkMinimumBalance(){
+    Account acc = new Account("12345",100);
+    assertThat(acc.doesHaveMinimumBalance(),is(false));
+    Account acc2 = new Account("12345",10000);
+    assertThat(acc2.doesHaveMinimumBalance(),is(true));
+  }
+  @Test
+  public void checkCredit() {
+    Account acc = new Account("12345", 10000);
+    acc.credit(1000);
+    assertThat(acc.getBalance(), is(11000.0));
   }
 }
